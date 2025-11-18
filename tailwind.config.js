414965c33ebe81c730d9bfafee841adc17647cd2
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-11-17 22:43:05 -0500

    feat: Add custom color palette and improve CSS structure for better styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          500: '#1c8da6',
+          700: '#165873',
+        },
+        neutral: {
+          0: '#ffffff',
+          200: '#d9d9d9',
+          300: '#c2c2c2',
+          900: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 }
